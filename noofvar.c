5799248952922ad1eee1ab75cfdf8219c9fda959
BALAJI M <balajimvl@gmail.com>
2016-03-05 13:06:44 +0530

    Update noofvar.c

--- noofvar.c
+++ noofvar.c
@@ -4,6 +4,7 @@ int main()
 	int a[26]={0};
 	int count[10]={0},i,d,c=0,n;
 	char str[10];
+	printf("Enter the String\n");
 	scanf("%s",str);
 	for(n=0;str[n]!='\0';n++);
 	for(i=0;i<n;i++)
@@ -18,6 +19,6 @@ int main()
 			}
 		}
 	}
-	printf("%d\n",c);
+	printf("The Number of variables in %s is %d\n",str,c);
 	return 0;
 }
